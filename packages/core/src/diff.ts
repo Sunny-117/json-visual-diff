13deb808c9b9659409739ccd3aaac19615c04559
Sunny-117 <zhiqiangfu6@gmail.com>
2026-02-09 15:20:51 +0800

    chore: format

--- packages/core/src/diff.ts
+++ packages/core/src/diff.ts
@@ -292,10 +292,7 @@ export class DiffEngine {
             break;
           case "modify":
             // 元素被修改，递归比较内部差异
-            const modifyChildDiff = this.diff(op.value, op.newValue, [
-              ...path,
-              String(newIndex),
-            ]);
+            const modifyChildDiff = this.diff(op.value, op.newValue, [...path, String(newIndex)]);
             children.push(modifyChildDiff);
             oldIndex++;
             newIndex++;
